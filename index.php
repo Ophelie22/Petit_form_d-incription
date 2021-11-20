b7670f8c509c8d9e2b45cb8cd04452a4c10d5de3
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-11-20 21:23:35 +0100

    creation de notre code et des verifications de securité avec hasd et cryptage

--- index.php
+++ index.php
@@ -1,5 +1,53 @@
 <?php
+require('src/connect.php');
+// on va verifie si nos variables existent et si il bien rempli(!empty)
+if(!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password'])
+ && !empty($_POST['password_confirm'])){
+     //echo $_['pseudo'];
 
+     // VARIABLE
+     $pseudo       = $_POST['pseudo'];
+     $email        = $_POST['email'];
+     $password     = $_POST['password'];
+     $pass_confirm = $_POST['password_confirm'];
+ 
+     // TEST SI PASSWORD = PASSWORD CONFIRM
+     if ($password != $pass_confirm) {
+         header('Location: index.php?error=1&pass=1');//systeme d'erreur. 
+         //On va rajouter une condition ds notre html
+         exit();
+     }
+ 
+		// TEST SI EMAIL UTILISE
+    //on utilise prepare pour securise notre formulaire et eviter l'injection
+		$req = $db->prepare("SELECT count(*) as numberEmail FROM users WHERE email = ?");
+		$req->execute(array($email));
+    //on va utiliser une boucle pour verifier avec fetch qui va verifier 
+    //un element par un element à la fois
+ 		while($email_verification = $req->fetch()){
+			if($email_verification['numberEmail'] != 0) {
+				header('location: index.php?error=1&email=1');
+				exit();
+ 			}
+		}
+    // encryptage HASH le sha1 va hasher le mdp et le time 
+    //va rajouter une suite de nombre egalement
+ 		$secret = sha1($email).time();
+		$secret = sha1($secret).time().time();
+
+    // CRYPTAGE DU PASSWORD on va rajouter notre grain pour la secu.
+    //aq1 suivi de l'encyptage associe à 1254 et 25 a la fin pour hash.
+ 		$password = "aq1".sha1($password."1254")."25";
+ 
+    // ENVOI DE LA REQUETE
+ 		$req = $db->prepare("INSERT INTO users(pseudo, email, password, secret) VALUES(?,?,?,?)");
+		$value = $req->execute(array($pseudo, $email, $password, $secret));
+			
+		header('location: ../?success=1');
+		exit();
+ };
+
+ 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +63,22 @@
     <h1> Inscris toi </h1>
   </header>
   <div class ="container">
+    <?php
+		 //si une erreur est detectée
+			if(isset($_GET['error'])){
+		 
+				if(isset($_GET['pass'])){
+					echo '<p id="error">Les mots de passe ne correspondent pas.</p>';
+				}
+				else if(isset($_GET['email'])){
+					echo '<p id="error">Cette adresse email est déjà utilisée.</p>';
+				}
+			}
+			else if(isset($_GET['success'])){
+				echo '<p id="success">Inscription prise correctement en compte.</p>';
+			}
+		 
+		?>
      <p id="info"> Tu es de jà inscris c'est par ici :<a href="connection.php">Connectes-toi</a>
      <br>
       Bienvenue ce petit exemple de formulaire te serviras de bases pour le reste <br>
